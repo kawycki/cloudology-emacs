48a96957a73f5f90f4d09dae536192c1eafa640b
Kamil Wydrzycki <kamwyd@gmail.com>
2019-10-12 20:34:27 +0200

    Eshell config.

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -24,12 +24,32 @@
 
 ;;; Code:
 
+(use-package eshell
+  :ensure nil
+  :commands eshell/alias
+  :hook (eshell-mode . (lambda ()
+                         (eshell/alias "f" "find-file $1")
+                         (eshell/alias "d" "dired $1")
+                         (eshell/alias "ll" "ls -l")
+                         (eshell/alias "la" "ls -al")))
+  :config
+  (setq eshell-scroll-to-bottom-on-input 'all
+        eshell-scroll-to-bottom-on-output 'all))
+
 (use-package eshell-toggle
-  :demand t
+  :commands ehsell-toggle
   :bind (("C-'" . eshell-toggle))
   :config
-  (setq eshell-toggle-run-command nil
+  (setq eshell-modules-list (remq 'eshell-banner eshell-modules-list)
+        eshell-toggle-run-command nil
         eshell-toggle-size-fraction 2))
+
+(use-package eshell-prompt-extras
+  :commands epe-theme-lambda
+  :defines eshell-highlight-prompt eshell-prompt-function
+  :init
+  (setq eshell-highlight-prompt nil
+        eshell-prompt-function 'epe-theme-lambda))
 
 (provide 'init-shell)
 ;;; init-shell.el ends here
