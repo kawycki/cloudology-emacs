19738b36066f3c64a913e0c483c2fd83934434bf
Kamil Wydrzycki <kawz@demant.com>
2019-11-11 14:54:55 +0100

    Goto the point of the most recent edit in the buffer.

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -33,6 +33,9 @@
         avy-style 'at-full
         avy-timeout-seconds 0.8))
 
+(use-package goto-chg
+  :bind ("C-," . goto-last-change))
+
 (use-package smartparens
   :hook (after-init . smartparens-global-mode)
   :bind (("<M-right>" . sp-forward-sexp)
