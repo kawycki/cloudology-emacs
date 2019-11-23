a2b9eef7017fea603ed1cf10850aa615675c0ae8
Kamil Wydrzycki <kawz@demant.com>
2019-11-23 08:41:34 +0100

    Custom eshell prompt.

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -41,11 +41,24 @@
   :commands ehsell-toggle
   :bind (("C-'" . eshell-toggle))
   :config
-  (setq eshell-modules-list (remq 'eshell-banner eshell-modules-list)
+  (defun default-prompt-fn ()
+    (require 'shrink-path)
+    (concat (if (bobp) "" "\n")
+            (let ((pwd (eshell/pwd)))
+              (propertize (if (equal pwd "~")
+                              pwd
+                            (abbreviate-file-name (shrink-path-file pwd)))
+                          'face '+eshell-prompt-pwd))
+            (propertize " λ" 'face (if (zerop eshell-last-command-status) 'success 'error))
+            " "))
+
+  (setq eshell-prompt-function 'default-prompt-fn
+        eshell-modules-list (remq 'eshell-banner eshell-modules-list)
         eshell-toggle-run-command nil
         eshell-toggle-size-fraction 2))
 
 (use-package eshell-prompt-extras
+  :disabled t
   :commands epe-theme-lambda
   :defines eshell-highlight-prompt eshell-prompt-function
   :init
