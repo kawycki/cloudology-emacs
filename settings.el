b0cc6056b1ca05c13bbe4a0da307787474af5820
Kamil Wydrzycki <kamwyd@gmail.com>
2020-03-12 14:02:29 +0100

    Removal of custom font color.

--- settings.el
+++ settings.el
@@ -13,7 +13,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#242730" :foreground "#bbc2cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 108 :width normal :foundry "outline" :family "Fira Code"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "Hack"))))
  '(aw-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 3.0))))
  '(aw-mode-line-face ((t (:inherit mode-line-emphasis :bold t))))
  '(flycheck-error ((t (:underline "#ff665c"))))
